3e5683cf03488cb7641b9dc284353b1b9f8f8385
Pankaj Singh Sirari <pankajsirari222@gmail.com>
2023-02-04 23:44:18 +0530

    initial commit

--- class29.java
+++ class29.java
@@ -1,3 +1,30 @@
+import java.util.*;
+
+/*
+ *  Queue is used for BFS. 
+ */
 public class class29 {
-    
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        if(root == null)    return list;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        
+        while(!q.isEmpty()) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            int size = q.size();
+            
+            for(int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+
+                if(node.left != null)   q.offer(node.left);
+                if(node.right != null)   q.offer(node.right);
+                
+                arr.add(node.val);
+            }
+            
+            list.add(arr);
+        }
+        return list;
+    }
 }
